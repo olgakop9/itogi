356a63249885b129b267f38bdc3449159b5a88fb
Olga <Olga_kopylova9@mail.ru>
2022-11-12 19:34:39 +0500

    решение

---
+++ ex4/Program.cs
@@ -0,0 +1,41 @@
+﻿
+string[] array = AskArray();
+string[] result = FindLessThan(array, 3);
+Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] FindLessThan(string[] input, int n)
+{
+    string[] output = new string[CountLessThan(input, n)];
+
+    for (int i = 0, j = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+int CountLessThan(string[] input, int n)
+{
+    int count = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] AskArray()
+{
+    Console.Write("Введите значения через пробел: ");
+    return Console.ReadLine().Split(" ");
+}
